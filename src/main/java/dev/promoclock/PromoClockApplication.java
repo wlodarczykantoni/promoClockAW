b27e800c4f24cbd0903c8cc2ebcc7d4760aaa4de
Antek <xbbshshxhz@gmail.com>
2023-11-24 11:18:18 +0100

    odlicznie zrobione

--- src/main/java/dev/promoclock/PromoClockApplication.java
+++ src/main/java/dev/promoclock/PromoClockApplication.java
@@ -1,3 +1,4 @@
+
 package dev.promoclock;
 
 import dev.promoclock.dev.promoclock.user.User;
@@ -11,22 +12,21 @@ import org.springframework.context.annotation.Bean;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Calendar;
 import java.util.Scanner;
-
 @SpringBootApplication
 public class PromoClockApplication extends User {
 
     private static final Logger log = LoggerFactory.getLogger(PromoClockApplication.class);
 
-
     public static void main(String[] args) {
         SpringApplication.run(PromoClockApplication.class, args);
-
     }
 
-    // Spring to odpali po starcie aplikacji
     @Bean
     public CommandLineRunner initDemoDb(dev.promoclock.user.UserRepository userRepository) {
         return (args) -> {
@@ -38,7 +38,6 @@ public class PromoClockApplication extends User {
             userRepository.save(user1);
             userRepository.save(user2);
 
-            // find all users
             log.info("findAll(), expect 3 users");
             log.info("-------------------------------");
             for (User user : userRepository.findAll()) {
@@ -46,55 +45,72 @@ public class PromoClockApplication extends User {
             }
             log.info("\n");
 
+            // pobieranie aktualnej daty i czas
+            LocalDateTime teraz = LocalDateTime.now();
 
-            System.out.print("tu bedzie licznik:");
-            //zabrane z jakiegos kursu
-            Date akutalnaGodzina = new Date();
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("YYYY-mm-dd HH:mm:ss");
-            System.out.println(simpleDateFormat.format(akutalnaGodzina));
+            // wypisanie aktualnej dsaty i czasu
+            System.out.println("Rok: " + teraz.getYear());
+            System.out.println("Miesiąc: " + teraz.getMonthValue());
+            System.out.println("Dzień: " + teraz.getDayOfMonth());
+            System.out.println("Godzina: " + teraz.getHour());
+            System.out.println("Minuta: " + teraz.getMinute());
+            System.out.println("Sekunda: " + teraz.getSecond());
 
-
-            // Pobieranie date i czas
-            Calendar now = Calendar.getInstance();
-
-            // Pobieranie wsszystkiego i nadanie wartosci
-            int rok = now.get(Calendar.YEAR);
-            int miesiac = now.get(Calendar.MONTH) + 1; // dodajemu 1 bo miesace sa od zera
-            int dzien = now.get(Calendar.DAY_OF_MONTH);
-            int godzina = now.get(Calendar.HOUR_OF_DAY);
-            int minuta = now.get(Calendar.MINUTE);
-
-            // wypisuejmy wszystkie
-            System.out.println("Rok: " + rok);
-            System.out.println("Miesiąc: " + miesiac);
-            System.out.println("Dzień: " + dzien);
-            System.out.println("Godzina: " + godzina);
-            System.out.println("Minuta: " + minuta);
-//sprawdzamy czy dziala
-            /*
-int rokNext = 10;
-int rokWant = rok + rokNext;
-System.out.println("rok ktory chcemy" + rokWant);
-*/
-
+            // Pobieranie danych od użytkownika
             Scanner scanner = new Scanner(System.in);
 
+            System.out.print("Podaj rok (większy lub równy aktualnemu): ");
+            int rokNext = scanner.nextInt();
+
+            System.out.print("Podaj miesiąc (1-12): ");
+            int miesiacNext = scanner.nextInt();
+
+            System.out.print("Podaj dzień (1-31): ");
+            int dzienNext = scanner.nextInt();
+
+            System.out.print("Podaj godzinę (0-23): ");
+            int godzinaNext = scanner.nextInt();
+
+            System.out.print("Podaj minutę (0-59): ");
+            int minutaNext = scanner.nextInt();
+
+            System.out.print("Podaj sekundy (0-59): ");
+            int sekundyNext = scanner.nextInt();
 
 
-                System.out.print("Podaj pierwszą liczbę: ");
-                int liczba1 = scanner.nextInt();
+            LocalDate podanaData = LocalDate.of(rokNext, miesiacNext, dzienNext);
+            LocalTime podanaGodzina = LocalTime.of(godzinaNext, minutaNext, sekundyNext);
 
-                System.out.print("Podaj drugą liczbę: ");
-                int liczba2 = scanner.nextInt();
+            if (podanaData.isBefore(teraz.toLocalDate()) || (podanaData.isEqual(teraz.toLocalDate()) && podanaGodzina.isBefore(teraz.toLocalTime()))) {
+                System.out.println("Błąd: Wprowadzona data i godzina muszą być większe lub równe aktualnej.");
+                return;
+            }
 
-                System.out.print("Wybierz operację:\n"
-                        + "1 - Dodawanie\n"
-                        + "2 - Odejmowanie\n"
-                        + "3 - Mnożenie\n"
-                        + "4 - Dzielenie\n");
+            if (sekundyNext < 0 || sekundyNext > 59 || minutaNext < 0 || minutaNext > 59 || godzinaNext < 0 || godzinaNext > 23 || dzienNext < 1 || dzienNext > 31) {
+                System.out.println("Błąd: Niepoprawne wartości dla sekund, minut, godzin lub dni.");
+                return;
+            }
+// liczenie jaka jest roznica czasu
+            int roznicaLat = Math.max(0, teraz.getYear() - podanaData.getYear());
+            int roznicaMiesiecy = Math.max(0, teraz.getMonthValue() - podanaData.getMonthValue() + 12 * roznicaLat);
+            int roznicaDni = Math.max(0, teraz.getDayOfMonth() - podanaData.getDayOfMonth());
+            int roznicaGodzin = Math.max(0, teraz.getHour() - godzinaNext);
+            int roznicaMinut = Math.max(0, teraz.getMinute() - minutaNext);
+            int roznicaSekund = Math.max(0, teraz.getSecond() - sekundyNext);
+
+// wypisanie akutalnej oczekiwanej i czas oczekiwania
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            System.out.println("Aktualna data: " + teraz.format(formatter));
+            System.out.println("Oczekiwana data: " + podanaData.atStartOfDay().plusHours(godzinaNext).plusMinutes(minutaNext).plusSeconds(sekundyNext).format(formatter));
+            System.out.println("Pozostały czas:");
+            System.out.println("  Lata: " + roznicaLat);
+            System.out.println("  Miesiące: " + roznicaMiesiecy);
+            System.out.println("  Dni: " + roznicaDni);
+            System.out.println("  Godziny: " + roznicaGodzin);
+            System.out.println("  Minuty: " + roznicaMinut);
+            System.out.println("  Sekundy: " + roznicaSekund);
 
 
         };
     }
 }
-
