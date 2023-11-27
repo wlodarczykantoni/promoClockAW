cc5dd1563b63b1370e732dfff45727237b2604b9
Antek <xbbshshxhz@gmail.com>
2023-11-27 12:06:37 +0100

    wrzucanie danych do html

--- src/main/java/dev/promoclock/HomeController.java
+++ src/main/java/dev/promoclock/HomeController.java
@@ -1,22 +1,54 @@
 package dev.promoclock;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 @Controller
 public class HomeController {
 
-    @GetMapping(value = "/")
-    public String index() {
-        return "index";
-    }
+    @GetMapping("/index/{data}")
+    public String clock(@PathVariable String data, Model model) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMyyyy");
+        LocalDateTime podanyCzas = LocalDateTime.of(LocalDate.parse(data, formatter), LocalTime.MIDNIGHT);
+        LocalDateTime teraz = LocalDateTime.now();
 
-    @GetMapping(value = "/welcome/{name}")
-    public String name(@PathVariable("name") String name, Model model) {
-        model.addAttribute("imie", name);
-        return "index";
+        // nadanie wartosc i liczenie ich
+        long roznicaLat = teraz.getYear() - podanyCzas.getYear();
+        long roznicaMiesiecy = teraz.getMonthValue() - podanyCzas.getMonthValue() + 12 * roznicaLat;
+        long roznicaDni = teraz.getDayOfMonth() - podanyCzas.getDayOfMonth();
+        long roznicaGodzin = teraz.getHour() - podanyCzas.getHour();
+        long roznicaMinut = teraz.getMinute() - podanyCzas.getMinute();
+        long roznicaSekund = teraz.getSecond() - podanyCzas.getSecond();
+
+        // aby nie bylo za duzo
+        if (roznicaSekund > 0) {
+            roznicaMinut++;
+            roznicaSekund %= 60;
+        }
+
+        if (roznicaMinut > 0) {
+            roznicaGodzin++;
+            roznicaMinut %= 60;
+        }
+
+        /// wrzucanie do html
+        model.addAttribute("aktualnaData", teraz.format(formatter));
+        model.addAttribute("oczekiwanaData", podanyCzas.format(formatter));
+        model.addAttribute("roznicaLat", roznicaLat);
+        model.addAttribute("roznicaMiesiecy", roznicaMiesiecy);
+        model.addAttribute("roznicaDni", roznicaDni);
+        model.addAttribute("roznicaGodzin", roznicaGodzin);
+        model.addAttribute("roznicaMinut", roznicaMinut);
+        model.addAttribute("roznicaSekund", roznicaSekund);
+
+        /// odpalenie  HTML
+        return "index.html";
     }
 }
--- src/main/java/dev/promoclock/PromoClockApplication.java
+++ src/main/java/dev/promoclock/PromoClockApplication.java
@@ -44,73 +44,7 @@ public class PromoClockApplication extends User {
                 log.info(user.toString());
             }
             log.info("\n");
-
-            // pobieranie aktualnej daty i czas
-            LocalDateTime teraz = LocalDateTime.now();
-
-            // wypisanie aktualnej dsaty i czasu
-            System.out.println("Rok: " + teraz.getYear());
-            System.out.println("Miesiąc: " + teraz.getMonthValue());
-            System.out.println("Dzień: " + teraz.getDayOfMonth());
-            System.out.println("Godzina: " + teraz.getHour());
-            System.out.println("Minuta: " + teraz.getMinute());
-            System.out.println("Sekunda: " + teraz.getSecond());
-
-            // Pobieranie danych od użytkownika
-            Scanner scanner = new Scanner(System.in);
-
-            System.out.print("Podaj rok (większy lub równy aktualnemu): ");
-            int rokNext = scanner.nextInt();
-
-            System.out.print("Podaj miesiąc (1-12): ");
-            int miesiacNext = scanner.nextInt();
-
-            System.out.print("Podaj dzień (1-31): ");
-            int dzienNext = scanner.nextInt();
-
-            System.out.print("Podaj godzinę (0-23): ");
-            int godzinaNext = scanner.nextInt();
-
-            System.out.print("Podaj minutę (0-59): ");
-            int minutaNext = scanner.nextInt();
-
-            System.out.print("Podaj sekundy (0-59): ");
-            int sekundyNext = scanner.nextInt();
-
-
-            LocalDate podanaData = LocalDate.of(rokNext, miesiacNext, dzienNext);
-            LocalTime podanaGodzina = LocalTime.of(godzinaNext, minutaNext, sekundyNext);
-
-            if (podanaData.isBefore(teraz.toLocalDate()) || (podanaData.isEqual(teraz.toLocalDate()) && podanaGodzina.isBefore(teraz.toLocalTime()))) {
-                System.out.println("Błąd: Wprowadzona data i godzina muszą być większe lub równe aktualnej.");
-                return;
-            }
-
-            if (sekundyNext < 0 || sekundyNext > 59 || minutaNext < 0 || minutaNext > 59 || godzinaNext < 0 || godzinaNext > 23 || dzienNext < 1 || dzienNext > 31) {
-                System.out.println("Błąd: Niepoprawne wartości dla sekund, minut, godzin lub dni.");
-                return;
-            }
-// liczenie jaka jest roznica czasu
-            int roznicaLat = Math.max(0, teraz.getYear() - podanaData.getYear());
-            int roznicaMiesiecy = Math.max(0, teraz.getMonthValue() - podanaData.getMonthValue() + 12 * roznicaLat);
-            int roznicaDni = Math.max(0, teraz.getDayOfMonth() - podanaData.getDayOfMonth());
-            int roznicaGodzin = Math.max(0, teraz.getHour() - godzinaNext);
-            int roznicaMinut = Math.max(0, teraz.getMinute() - minutaNext);
-            int roznicaSekund = Math.max(0, teraz.getSecond() - sekundyNext);
-
-// wypisanie akutalnej oczekiwanej i czas oczekiwania
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-            System.out.println("Aktualna data: " + teraz.format(formatter));
-            System.out.println("Oczekiwana data: " + podanaData.atStartOfDay().plusHours(godzinaNext).plusMinutes(minutaNext).plusSeconds(sekundyNext).format(formatter));
-            System.out.println("Pozostały czas:");
-            System.out.println("  Lata: " + roznicaLat);
-            System.out.println("  Miesiące: " + roznicaMiesiecy);
-            System.out.println("  Dni: " + roznicaDni);
-            System.out.println("  Godziny: " + roznicaGodzin);
-            System.out.println("  Minuty: " + roznicaMinut);
-            System.out.println("  Sekundy: " + roznicaSekund);
-
-
         };
+
     }
 }
